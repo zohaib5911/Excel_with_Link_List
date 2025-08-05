5c26f4d9d63ec7385d6291e924ffc65b08a39028
bscs23098 <zohaibsajjad129@gmail.com>
2025-08-05 13:04:54 +0500

    Formula v1 added

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ using namespace std;
 int main(){
 
     SpreadSheet<int> obj(5,5);
-    cout<<obj;
+    // cout<<obj;
 
     // cout<<"\nSet value\n";
-    // obj.setValue(3,3,33);
+    obj.setValue(3,3,33);
+    obj.setValue(4,4,44);
     // cout<<obj;
 
     // cout<<"\nGet value : \n";
     // cout <<"Value at 3 ,3  : "<<obj.getValue(3,3)<<endl;
 
-    obj.insertRow(0);
+    // obj.insertRow(0);
 
+    // obj.setValue(1,1,3,3,4,4);
+
+    obj.setValue("a1","b3","d4");
     cout<<endl<<obj<<endl;
     return 0;
 }
--- spreadSheet.h
+++ spreadSheet.h
@@ -10,7 +10,6 @@ template <typename T>
 class SpreadSheet{
     int rows,columns;
     Cell<T>* head;
-    
 public:
     SpreadSheet(int r,int col):rows(r),columns(col),head(nullptr){
        if(rows == 0 || columns == 0) return;
@@ -31,7 +30,46 @@ public:
         }
          head = grid[0][0];
     }
-
+    Cell<T>* getCell(int row,int col){
+        Cell<T>* temp = head;
+        for(int i=0;i<row;i++)  temp = temp->down;
+        for(int i=0;i<col;i++)  temp = temp->right;
+        return temp;
+    }
+    void setValue(string target,string t1,string t2){
+        int row_1 =  target[0] - 'a';
+        int col_1 = target[1] - '0';
+        int row_2 = t1[0] - 'a';
+        int col_2 = t1[1] - '0';
+        int row_3 = t2[0] - 'a';
+        int col_3 = t2[1] - '0';
+        cout<<row_1<<" _ "<<col_1;
+        setValue(row_1,col_1,row_2,col_2,row_3,col_3);
+    }    
+    void setValue(int row_1,int col_1,int row_2,int col_2,int row_3,int col_3){
+        Cell<T>* r = getCell(row_1,col_1);
+        Cell<T>* t1 = getCell(row_2,col_2);
+        Cell<T>* t2 = getCell(row_3,col_3);
+        if(r && t1 && t2){
+            char op;
+            cout<<"Enter sign (*,/,+,-) : ";
+            cin>>op;
+            switch (op)
+            {
+            case '+' : r->value = t1->value + t2->value;    break;
+            case '-' : r->value = t1->value - t2->value;    break;
+            case '*' : r->value = t1->value * t2->value;    break;
+            case '/' : 
+                if(t2->value == 0){
+                    cout<<"Error Divide is not possible\n";
+                }
+                r->value = t1->value / t2->value;
+                break;
+            default:
+                cout << "Invalid operator entered.\n";
+            }
+        }
+    }
     void setValue(int row, int col, T value) {
         Cell<T>* temp = head;
         for (int i = 0; i < row && temp; ++i) {
@@ -78,6 +116,7 @@ public:
             setValue(i,col,val);
         }
     }
+
 
     friend ostream& operator<<(ostream& os, const SpreadSheet& obj) {
         Cell<T>* row_ptr = obj.head;
